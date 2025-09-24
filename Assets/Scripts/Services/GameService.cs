fa46c05c0ee51d605aa759ab0054433a3b1f6762
Aditya Singh <92723657+Addy1317@users.noreply.github.com>
2025-09-24 20:24:54 +0530

    feat: Generics and Managers

---
+++ Assets/Scripts/Services/GameService.cs
@@ -0,0 +1,61 @@
+using SlowpokeStudio.Audio;
+using SlowpokeStudio.Currecny;
+using SlowpokeStudio.Event;
+using SlowpokeStudio.Generic;
+using SlowpokeStudio.Manager;
+using SlowpokeStudio.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SlowpokeStudio.Services
+{
+    public class GameService : GenericMonoSingleton<GameService>
+    {
+        [Header("Service")]
+        [SerializeField] internal AudioManager audioManager;
+        [SerializeField] internal CurrencyManager currencyManager;
+        [SerializeField] internal EventManager eventManager;
+        [SerializeField] internal GameManager gameManager;
+        [SerializeField] internal UIManager uiManager;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            if (Instance == this)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            InitializeServicesCheck();
+        }
+
+        private void InitializeServicesCheck()
+        {
+            var services = new Dictionary<string, Object>
+            {
+            { "GameManager", gameManager },
+            { "AudioManager", audioManager },
+            { "CurrencyManager", currencyManager },
+            { "EventManager", eventManager },
+            { "UIManager", uiManager },
+            };
+
+            foreach (var service in services)
+            {
+                if (service.Value == null)
+                {
+                    Debug.LogError($"{service.Key} failed to initialize.");
+                }
+                else
+                {
+                    Debug.Log($"{service.Key} is initialized.");
+                }
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,13 @@
+#region Summary
+// UIManager is responsible for managing and updating the game's UI elements, including health, wave count, kills, currency, and tower selection.
+// It updates the UI when certain events happen, such as enemy deaths or health changes.
+#endregion
+using UnityEngine;
+
+namespace SlowpokeStudio.UI
+{
+    public class UIManager : MonoBehaviour
+    {
+       
+    }
+}
